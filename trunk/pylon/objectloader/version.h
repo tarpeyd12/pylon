4d273e70196555f41f766e946645c28839bd9cbd
tarpeyd12 <tarpeyd12@c0da84c2-71d4-5f1e-1e30-951e400d58d2>
2011-05-23 22:07:48 +0000

    stooof

--- trunk/pylon/objectloader/version.h
+++ trunk/pylon/objectloader/version.h
@@ -2,32 +2,32 @@
 #define OBJECTLOADER_VERSION_H
 
 namespace ObjectLoaderAutoVersion{
-	
+
 	//Date Version Types
 	static const char DATE[] = "23";
 	static const char MONTH[] = "05";
 	static const char YEAR[] = "2011";
-	static const char UBUNTU_VERSION_STYLE[] = "11.05";
-	
+	static const double UBUNTU_VERSION_STYLE = 11.05;
+
 	//Software Status
 	static const char STATUS[] = "Alpha";
 	static const char STATUS_SHORT[] = "a";
-	
+
 	//Standard Version Type
 	static const long MAJOR = 0;
 	static const long MINOR = 0;
-	static const long BUILD = 19;
-	static const long REVISION = 100;
-	
+	static const long BUILD = 20;
+	static const long REVISION = 106;
+
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT = 225;
-	#define RC_FILEVERSION 0,0,19,100
-	#define RC_FILEVERSION_STRING "0, 0, 19, 100\0"
-	static const char FULLVERSION_STRING[] = "0.0.19.100";
-	
+	#define RC_FILEVERSION 0,0,20,106
+	#define RC_FILEVERSION_STRING "0, 0, 20, 106\0"
+	static const char FULLVERSION_STRING[] = "0.0.20.106";
+
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 19;
-	
+	static const long BUILD_HISTORY = 20;
+
 
 }
-#endif //OBJECTLOADER_VERSION_H
+#endif //VERSION_h
